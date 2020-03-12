665c71a56961b588cac8913352b5a5c54ffdfe5c
javaDeveloperZeng <1729817647@qq.com>
2020-03-13 07:49:21 +0800

    添加robbin

--- springCloudMain/springCloudConsumer/src/main/java/zla/springCloudConsumer/ApplicationConsumer.java
+++ springCloudMain/springCloudConsumer/src/main/java/zla/springCloudConsumer/ApplicationConsumer.java
@@ -2,12 +2,18 @@ package zla.springCloudConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import zla.springCloudConsumer.configuer.RibbonConfig;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@RibbonClient(name="service-hi",configuration= RibbonConfig.class)
 public class ApplicationConsumer
 {
     public static void main( String[] args )
--- springCloudMain/springCloudProvider/src/main/java/zla/springCloudProvider/ProviderApplication.java
+++ springCloudMain/springCloudProvider/src/main/java/zla/springCloudProvider/ProviderApplication.java
@@ -2,12 +2,15 @@ package zla.springCloudProvider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderApplication
 {
     public static void main( String[] args )
